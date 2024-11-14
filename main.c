2546bc7b484827988e1d35a42901956c17adbb54
Gabriel28112005 <gpenatam@myuax.com>
2024-11-14 13:14:13 +0100

    Commit 3: comando en windows

--- main.c
+++ main.c
@@ -2,30 +2,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+#include <windows.h>
 
 #define SIZE 4096
 
 int main() {
-    char *shared_memory = mmap(NULL, SIZE, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
-    if (shared_memory == MAP_FAILED) {
-        perror("mmap");
-        exit(EXIT_FAILURE);
+    HANDLE hMapFile;
+    char *shared_memory;
+
+    hMapFile = CreateFileMapping(
+        INVALID_HANDLE_VALUE,    // Use paging file
+        NULL,                    // Default security
+        PAGE_READWRITE,          // Read/write access
+        0,                       // Maximum object size (high-order DWORD)
+        SIZE,                    // Maximum object size (low-order DWORD)
+        "SharedMemory");         // Name of mapping object
+
+    if (hMapFile == NULL) {
+        printf("Could not create file mapping object (%d).\n", GetLastError());
+        return 1;
     }
 
-    pid_t pid = fork();
+    shared_memory = (char *)MapViewOfFile(hMapFile,   // Handle to map object
+                                          FILE_MAP_ALL_ACCESS, // Read/write permission
+                                          0,
+                                          0,
+                                          SIZE);
 
-    if (pid < 0) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    } else if (pid == 0) {
-        printf("Child reads: %s\n", shared_memory);
-        munmap(shared_memory, SIZE);
-        exit(EXIT_SUCCESS);
-    } else {
+    if (shared_memory == NULL) {
+        printf("Could not map view of file (%d).\n", GetLastError());
+        CloseHandle(hMapFile);
+        return 1;
+    }
+
+    STARTUPINFO si = { sizeof(si) };
+    PROCESS_INFORMATION pi;
+
+    if (CreateProcess(NULL, "child_process.exe", NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
+        // Parent process
         strcpy(shared_memory, "Hello, child process!");
-        wait(NULL);
-        munmap(shared_memory, SIZE);
+        WaitForSingleObject(pi.hProcess, INFINITE);
+        UnmapViewOfFile(shared_memory);
+        CloseHandle(hMapFile);
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+    } else {
+        // Child process
+        printf("Child reads: %s\n", shared_memory);
+        UnmapViewOfFile(shared_memory);
+        CloseHandle(hMapFile);
+        exit(0);
     }
 
     return 0;
